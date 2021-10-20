61ef7b88fdc66c556d194617b046f00b069401be
Benjamin Kowatsch <benjamin.kowatsch@gmail.com>
2021-10-20 11:31:53 +0200

    begin with transaction view

---
+++ frontend/src/views/transaction/TransactionList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="transaction-list container">
+    <h1>Transactions</h1>
+    <TransactionListEntry coin="BTC" amount=0.0302 transactionDate="2020-05-05"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TransactionListEntry from '@/components/transaction/TransactionListEntry.vue';
+
+export default {
+  name: 'TransactionList',
+  components: {
+    TransactionListEntry,
+  },
+};
+</script>
